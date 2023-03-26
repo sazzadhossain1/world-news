1810abc3c46d448c569738f98a0c9824b19706fe
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-03-27 04:07:08 +0600

    add carousel and navbar

--- src/Pages/Sharead/Header/LeftSideNav/RightSideNav/RightSideNav.js
+++ src/Pages/Sharead/Header/LeftSideNav/RightSideNav/RightSideNav.js
@@ -1,8 +1,17 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
-import { FaGithub, FaGoogle } from "react-icons/fa";
+import {
+  FaFacebookF,
+  FaGithub,
+  FaGoogle,
+  FaTwitch,
+  FaTwitter,
+  FaWhatsapp,
+  FaYoutube,
+} from "react-icons/fa";
 import ListGroup from "react-bootstrap/ListGroup";
+import BrandCarousel from "../../../BrandCarousel/BrandCarousel";
 
 const RightSideNav = () => {
   return (
@@ -16,14 +25,30 @@ const RightSideNav = () => {
         </Button>
       </ButtonGroup>
       <div>
-        <h5>Find Us On</h5>
+        <h5 className="my-3">Find Us On</h5>
         <ListGroup>
-          <ListGroup.Item>Cras justo odio</ListGroup.Item>
-          <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-          <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-          <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-          <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+          <ListGroup.Item className="mb-4 p-1">
+            <FaFacebookF className="mx-2"></FaFacebookF> Facebook
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-4 p-1">
+            <FaTwitter className="mx-2"></FaTwitter>
+            Twitter
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-4 p-1">
+            <FaWhatsapp className="mx-2"></FaWhatsapp>Whatsapp
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-4 p-1">
+            <FaTwitch className="mx-2"></FaTwitch>
+            Twitch
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-4 p-1">
+            <FaYoutube className="mx-2"></FaYoutube> Youtube
+          </ListGroup.Item>
         </ListGroup>
+      </div>
+
+      <div>
+        <BrandCarousel></BrandCarousel>
       </div>
     </div>
   );
